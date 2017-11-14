0fdd92e3b79344e1a6156447e402bad7ed50d211
[digitalapnea] <[coreya@gmail.com]>
2017-11-15 10:50:31 +1100

    Round 5.1

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello GCP CD - round 5!")
+	io.WriteString(w, "Hello GCP CD - round 5.1!")
 }
 
 func main() {

5a188e55f6bea98c1668a0bdc802c905f3eaa43f
[digitalapnea] <[coreya@gmail.com]>
2017-11-15 15:43:50 +1100

    This is 16.4

--- main.go
+++ main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func hello(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Hello GCP CD - let's do this v16.3!")
+	io.WriteString(w, "Hello GCP CD - let's do this v16.4!")
 }
 
 func main() {
